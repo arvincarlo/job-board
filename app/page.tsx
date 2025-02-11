0b645a54b26d37a32cb3744902e35fba96c08b4c
Arvin Cris <arvincris97@gmail.com>
2025-02-12 00:13:38 +0800

    Active states

--- app/page.tsx
+++ app/page.tsx
@@ -37,12 +37,12 @@ export default function Page() {
               {filters.map((item:string) => (
                 <div key={item}>
                   <span className="inline-block bg-light-grayish-cyan-tablets rounded-tl-md rounded-bl-md px-2 py-1 text-base font-semibold text-primary focus:bg-primary focus:text-white">{item}</span>
-                  <button onClick={() => dispatch({type:"filters/unset", payload: item})} className="text-white bg-primary px-[10px] py-1 rounded-tr-md rounded-br-md font-semibold">x</button>
+                  <button onClick={() => dispatch({type:"filters/unset", payload: item})} className="text-white bg-primary px-[10px] py-1 rounded-tr-md rounded-br-md font-semibold hover:bg-darkgray focus:bg-darkgray">x</button>
                 </div>
               ))}
             </div>
             <div>
-              <button onClick={() => dispatch({type:"filters/clear"})} className="text-custom-grayish-cyan">Clear</button>
+              <button onClick={() => dispatch({type:"filters/clear"})} className="text-custom-grayish-cyan focus:underline hover:underline">Clear</button>
             </div>
           </div>}
           {/* Jobs */}
