c75a3ffd541dc09c9c06b6d4ad5c2a5d397e446a
Arvin Cris <arvincris97@gmail.com>
2025-02-11 17:37:45 +0800

    Implement the Card Design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'custom-light-cyan': 'hsl(180, 52%, 96%)',
       },
       backgroundImage: {
         'mobile-image': "url('/images/bg-header-mobile.svg')",
