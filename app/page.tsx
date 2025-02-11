0b76c8800a024caed773b5ff6b650d221579c3ce
Arvin Cris <arvincris97@gmail.com>
2025-02-11 20:47:54 +0800

    Updated codes and added focus states

--- app/page.tsx
+++ app/page.tsx
@@ -33,15 +33,15 @@ export default async function Page() {
             <li key={job.id}>{job.position}</li>
           ))}
         </ul> */}
-        <div className="pt-[172px] px-6 lg:mx-36">
+        <div className="pt-[172px] lg:pt-48 px-6 lg:mx-36">
           {jobs.map((job: Job) => (
             <div key={job.id} className={`lg:flex lg:items-center lg:justify-between mt-10 max-w-sm lg:max-w-full rounded overflow-hidden bg-white shadow-lg p-6 ${job.new || job.featured ? 'border-l-[5px] border-l-primary' : null}`}>
-              <div className="flex grow items-center mb-4">
+              <div className="flex grow items-center mb-4 lg:gap-2">
                 <div className="relative aspect-square h-12 w-12 lg:h-24 lg:w-24">
                   <Image fill className="object-cover" src={job.logo} alt={`${job.id}`} />
                 </div>
                 <div className="ml-4">
-                  <div className="flex gap-2">
+                  <div className="flex gap-2 lg:gap-4">
                     <div className="text-primary font-medium text-base lg:text-lg">{job.company}</div>
 
                       <div className="flex items-center">
@@ -49,7 +49,7 @@ export default async function Page() {
                         {job.featured && <span className="inline-block bg-darkgray rounded-full px-2 py-[2px] text-sm  text-white">FEATURED</span>}
                       </div>
                   </div>
-                  <div className="text-gray-600 font-semibold text-lg lg:text-2xl">{job.position}</div>
+                  <div className="text-gray-600 font-semibold text-lg lg:text-2xl hover:text-primary hover:cursor-pointer">{job.position}</div>
                   <div className="flex space-x-2 text-custom-grayish-cyan text-base lg:text-lg">
                     <span>{job.postedAt}</span>
                     <span>-</span>
@@ -63,12 +63,11 @@ export default async function Page() {
               <div className="flex border-t border-gray-200 mt-2 pt-4">
               </div>
               <div className="flex gap-2 flex-wrap">
-                {job.languages.map(language => (
-                  <span key={language} className="inline-block bg-light-grayish-cyan-tablets  rounded px-2 py-1 text-base font-semibold text-primary mr-2">{language}</span>
-                ))}
-                {job.tools.map(tool => (
-                  <span key={tool} className="inline-block bg-light-grayish-cyan-tablets rounded px-2 py-1 text-base font-semibold text-primary mr-2">{tool}</span>
-                ))}
+                {
+                  [...job.languages, ...job.tools].map(item => (
+                    <button key={item} className="inline-block bg-light-grayish-cyan-tablets rounded px-2 py-1 text-base font-semibold text-primary mr-2 focus:bg-primary focus:text-white">{item}</button>
+                  ))
+                }
               </div>
             </div>
           ))}
